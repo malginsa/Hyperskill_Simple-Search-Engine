d9cbf6e4ec3d384d59ad13e33f4c1a116169a3c9
Sergei Malgin <malginsa@gmail.com>
2020-12-24 00:29:22 +0300

    Stage6 is added: added test for ANY strategy

---
+++ src/test/java/search/stage6/AnyStrategyTest.java
@@ -0,0 +1,58 @@
+package search.stage6;
+
+//import org.junit.Assert;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class AnyStrategyTest {
+
+    @Test
+    void shouldFindFourPerson() {
+        Strategy strategy = new AnyStrategy();
+        strategy.setAllPeople(List.of(
+                "Dwight Joseph djo@gmail.com",
+                "Rene Webb webb@gmail.com",
+                "Katie Jacobs",
+                "Erick Harrington harrington@gmail.com",
+                "Myrtle Medina",
+                "Erick Burgess"));
+        strategy.setQueries(List.of(
+                "Erick",
+                "Dwight",
+                "webb@gmail.com"));
+        List<String> expected = List.of(
+                "Erick Harrington harrington@gmail.com",
+                "Erick Burgess",
+                "Dwight Joseph djo@gmail.com",
+                "Rene Webb webb@gmail.com");
+        strategy.buildIndecies();
+        List<String> actual = strategy.execute();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindThreePerson() {
+        Strategy strategy = new AnyStrategy();
+        strategy.setAllPeople(List.of(
+                "Dwight Joseph djo@gmail.com",
+                "Rene Webb webb@gmail.com",
+                "Katie Jacobs",
+                "Erick Harrington harrington@gmail.com",
+                "Myrtle Medina",
+                "Erick Burgess"));
+        strategy.setQueries(List.of(
+                "Katie",
+                "Erick",
+                "QQQ"));
+        List<String> expected = List.of(
+                "Katie Jacobs",
+                "Erick Harrington harrington@gmail.com",
+                "Erick Burgess");
+        strategy.buildIndecies();
+        List<String> actual = strategy.execute();
+        Assert.assertEquals(expected, actual);
+    }
+}
